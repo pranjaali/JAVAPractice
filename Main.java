c33737b60113021693802ab81fdbd3786a889de0
pranjaali <pranjalisathe390@gmail.com>
2022-06-18 19:23:10 +0530

    practice -18/6/2022

--- Main.java
+++ Main.java
@@ -1,30 +1,47 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
+import java.util.List;
 class Main{
   public static void main(String[] args)
   {
-    Scanner in=new Scanner(System.in);
-    int target=in.nextInt();
-    int[] array={1,2,3,4,15,44,56};
-    int ans=fun(array,0,array.length,target);
-    System.out.print(ans);
-  }
-  static int fun(int[] array,int s,int e,int target)
-  {
-    if(s>e){
-      return -1;
-    }
-    int m=s+(e-s)/2;
-    if(array[m]==target)
-    {
-      return m;
-    }
-    if(array[m]>target)
-    {
-      return(fun(array,s,m-1,target));
-    }
-          return(fun(array,m+1,e,target));
+    System.out.print("Enter 2D array size : ");
+    Scanner sc=new Scanner(System.in);
+    // int rows=sc.nextInt();/
+    int columns=sc.nextInt();
     
+    System.out.println("Enter array elements : ");    
+     
+    int twoD[]=new int[columns];
+     
+       
+               
+         for(int j=0; j<columns;j++)
+         {
+             twoD[j]=sc.nextInt();
+         }
+      
 
+      int ans=maxx(twoD);
+      System.out.println(ans);
+  }
+  static int maxx(int[] candies)
+  { 
+    int[] arr=new int[candies.length];
+    for(int i=0;i<candies.length;i++)
+    {
+      for(int j=0;j<candies.length;j++)
+      {
+        if(candies[i]>candies[j])
+        {
+          arr[i]++;
+        }
+      }
+    }
+    for(int i=0;i<candies.length;i++)
+    {
+      System.out.println("("+arr[i]+")");
+    }
+
+    return 1;
   }
 }
