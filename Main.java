6ca0cc3c130c754db8c212d8cd18637157e43fe1
pranjaali <pranjalisathe390@gmail.com>
2022-06-22 22:39:39 +0530

    practice

--- Main.java
+++ Main.java
@@ -1,23 +1,12 @@
-import java.util.*;
+import java.util.Scanner;
 public class Main{
-  class MyGeneric<T1>{
-     int val;
-     private T1 t1;
-     public MyGeneric(int val12,T1 t12)
-     {
-       t1=t12;
-       val=val12;
-     }
-     public int getVal() {
-         return val;
-     }
-     public T1 gett1()
-     {
-       return t1;
-     }
-  }
   public static void main(String[] args)
   {
-    MyGeneric<String> g1=new MyGeneric<String>(2, "sdb");
+    String ar=new String("hellow");
+    String ne=new String("worls");
+    int a=99;
+    int b=319;
+    System.out.println(""+a+b);
+     
   }
 }
