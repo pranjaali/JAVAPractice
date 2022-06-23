086bd495c4bb22060a8d5db3b4bc5fe1c6eed9b7
pranjaali <pranjalisathe390@gmail.com>
2022-06-23 14:10:23 +0530

    insertion sort

--- Main.java
+++ Main.java
@@ -1,12 +1,35 @@
-import java.util.Scanner;
+import java.util.*;
 public class Main{
   public static void main(String[] args)
   {
-    String ar=new String("hellow");
-    String ne=new String("worls");
-    int a=99;
-    int b=319;
-    System.out.println(""+a+b);
-     
+    int[] arr={3,1,0,3,4,2,55,7};
+    int temp=0;
+    int max=arr[0];
+    int cnt=arr.length;
+    int len=arr.length;
+    for(int j=0;j<len-1;j++)
+    {
+      for(int i=0;i<len-1-j;i++)
+      {
+  
+        if(arr[i]>arr[i+1])
+        {
+  
+          temp=arr[i];
+          arr[i]=arr[i+1];
+          arr[i+1]=temp;
+
+          // len--;
+        }
+        max=arr[i];
+        
+      }
+      
+    }
+    printArray(arr);
+    // for(int j=0;j<arr.length;j++)
+    // {
+    //   System.out.print(" "+arr[j]);
+    // }
   }
 }
