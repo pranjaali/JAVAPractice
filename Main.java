dd29aa32e88ad1cddc6599b82eedcef476a50c99
pranjaali <pranjalisathe390@gmail.com>
2022-06-24 00:02:56 +0530

    missing value

--- Main.java
+++ Main.java
@@ -2,21 +2,24 @@ import java.util.*;
 public class Main{
   public static void main(String[] args)
   {
-    int[] arr={90,0,3,4,2,55,7};
-     for(int i=1;i<arr.length;i++)
-     {
-       int j=i-1;
-       int cur=arr[i];
-       while(j>=0 && cur>arr[j])
-       {
-         arr[j+1]=arr[j];
-         j--;
-       }
-       arr[j+1]=cur;
-     }
-    for(int j=0;j<arr.length;j++)
-    {
-      System.out.print(" "+arr[j]);
-    }
+    int[] nums={3,0,1};
+     int num=missingNumber(nums);
+     System.out.println(num);
   }
+  static int missingNumber(int[] nums) {
+    int ans=0;
+    int[] ne=new int[nums.length+1];
+    for(int i=0;i<nums.length;i++)
+    {
+        ne[nums[i]]=1;
+    }
+    for(int i=0;i<nums.length+1;i++)
+    {
+       if(ne[i]!=1)
+       {
+           return i;
+       }
+    }
+    return nums.length;
+}
 }
