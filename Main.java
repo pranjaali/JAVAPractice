0be62a71ab13422b06f0abdd728f527c9e865868
pranjaali <pranjalisathe390@gmail.com>
2022-06-23 15:27:59 +0530

    selection sort

--- Main.java
+++ Main.java
@@ -2,34 +2,29 @@ import java.util.*;
 public class Main{
   public static void main(String[] args)
   {
-    int[] arr={3,1,0,3,4,2,55,7};
-    int temp=0;
-    int max=arr[0];
-    int cnt=arr.length;
-    int len=arr.length;
-    for(int j=0;j<len-1;j++)
+    int[] arr={90,0,3,4,2,55,7};
+    // int temp=0;
+   for(int i=0;i<arr.length-1;i++)
+   {
+     int smallestIndex=i;
+     for(int j=i+1;j<arr.length;j++)
+     {
+         if(arr[smallestIndex]>arr[j])
+         {
+           smallestIndex=j;
+         }
+     }
+    //  arr[smallestIndex]=arr[smallestIndex]^arr[i];
+    //  arr[i]=arr[smallestIndex]^arr[i];
+    //  arr[smallestIndex]=arr[smallestIndex]^arr[i];
+     int temp=arr[smallestIndex];
+     arr[smallestIndex]=arr[i];
+     arr[i]=temp;
+   }
+    // printArray(arr);
+    for(int j=0;j<arr.length;j++)
     {
-      for(int i=0;i<len-1-j;i++)
-      {
-  
-        if(arr[i]>arr[i+1])
-        {
-  
-          temp=arr[i];
-          arr[i]=arr[i+1];
-          arr[i+1]=temp;
-
-          // len--;
-        }
-        max=arr[i];
-        
-      }
-      
+      System.out.print(" "+arr[j]);
     }
-    printArray(arr);
-    // for(int j=0;j<arr.length;j++)
-    // {
-    //   System.out.print(" "+arr[j]);
-    // }
   }
 }
