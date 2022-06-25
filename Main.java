af9747777a4c4fb6f3158bc93dbdac0ffc6cf253
pranjaali <pranjalisathe390@gmail.com>
2022-06-25 23:19:10 +0530

    25/06/2022

--- Main.java
+++ Main.java
@@ -23,28 +23,28 @@ public class Main{
     return nums.length;
 }
 }
-public class Solution {
-  public int maximumProduct(int[] nums) {
-      int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
-      int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
-      for (int n: nums) {
-          if (n <= min1) {
-              min2 = min1;
-              min1 = n;
-          } else if (n <= min2) {     // n lies between min1 and min2
-              min2 = n;
-          }
-          if (n >= max1) {            // n is greater than max1, max2 and max3
-              max3 = max2;
-              max2 = max1;
-              max1 = n;
-          } else if (n >= max2) {     // n lies betweeen max1 and max2
-              max3 = max2;
-              max2 = n;
-          } else if (n >= max3) {     // n lies betwen max2 and max3
-              max3 = n;
+class Solution {
+  public String interpret(String command) {
+//          creating new object s to make a mutable string 
+     StringBuilder S = new StringBuilder(command.length());
+      int i=0;
+      while(i<command.length()){
+          if(command.charAt(i)=='G'){
+              S.append('G');
+              i++;
+//                 checking for () or (al)
+          }else if(command.charAt(i)=='('){
+              if(command.charAt(i+1)=='a'){
+                    S.append("al");
+              i=i+4;
+              
+              }else{
+                      S.append('o');
+              i+=2; 
+              }
+         
           }
       }
-      return Math.max(min1 * min2 * max1, max1 * max2 * max3);
+      return S.toString();
   }
 }
