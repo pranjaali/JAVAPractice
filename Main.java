d61f811eb0be9f62531b04e70a40a41da4afc1fa
pranjaali <pranjalisathe390@gmail.com>
2022-06-27 23:36:58 +0530

    27/06/2022

--- Main.java
+++ Main.java
@@ -1,50 +1,44 @@
 import java.util.*;
-public class Main{
+class Main{
   public static void main(String[] args)
   {
-    int[] nums={3,0,1};
-     int num=missingNumber(nums);
-     System.out.println(num);
+    Scanner in=new Scanner(System.in);
+    int num=in.nextInt();
+    int[] arr=new int[num];
+    System.out.println("Enter the array elements:");
+    for(int i=0;i<arr.length;i++)
+    {
+       arr[i]=in.nextInt();
+    }
+    Solution s=new Solution();
+    s.fun(arr);
+
+
   }
-  static int missingNumber(int[] nums) {
-    int ans=0;
-    int[] ne=new int[nums.length+1];
-    for(int i=0;i<nums.length;i++)
-    {
-        ne[nums[i]]=1;
-    }
-    for(int i=0;i<nums.length+1;i++)
-    {
-       if(ne[i]!=1)
-       {
-           return i;
-       }
-    }
-    return nums.length;
 }
-}
-class Solution {
-  public String interpret(String command) {
-//          creating new object s to make a mutable string 
-     StringBuilder S = new StringBuilder(command.length());
-      int i=0;
-      while(i<command.length()){
-          if(command.charAt(i)=='G'){
-              S.append('G');
-              i++;
-//                 checking for () or (al)
-          }else if(command.charAt(i)=='('){
-              if(command.charAt(i+1)=='a'){
-                    S.append("al");
-              i=i+4;
-              
-              }else{
-                      S.append('o');
-              i+=2; 
-              }
-         
-          }
+class Solution{
+  public void fun(int[] arr)
+  {
+    int temp=0;
+    for(int i=0;i<arr.length;i++)
+    {
+      // temp=arr[i-1];
+      for(int j=i+1;j<arr.length;j++)
+      {
+        if(arr[i]>arr[j])
+        {
+          temp=arr[i];
+          arr[i]=arr[j];
+          arr[j]=temp;
+        }
+        Arrays.sort(arr);
+        for(int k=0;k<arr.length;k++)
+        {
+          System.out.print(arr[k]);
+        }
+        System.out.println(" ");
+       
       }
-      return S.toString();
+    }
   }
 }
