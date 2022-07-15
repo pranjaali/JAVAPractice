de20fcd7ffb1f64000e14413e1772a0e157f1778
pranjaali <pranjalisathe390@gmail.com>
2022-07-15 16:21:29 +0530

    Stack using Queue

--- Main.java
+++ Main.java
@@ -1,52 +1,67 @@
 import java.util.*;
 public class Main{
-
-    static class Fun{
-        List<Integer>list=new ArrayList<>();
-        Fun(int[] arr)
-        {
-            for(int i=0;i<arr.length;i++)
-            {
-                list.add(arr[i]);
-            }
-            Collections.sort(list);
-           System.out.println(list);
-
-        }
-        int fun(int i)
-        {
-            int nas=-1;
-            if(i%2==0)
-            {
-              nas=  list.get(0);
-            list.remove(0);
-            }
-            if(i%2!=0)
-            {
-                nas=list.get(list.size()-1);
-                list.remove(list.size()-1);
-            }
-            return nas;
-        }
-    }
-
-
-
+   static class MyStack {
+    static Queue<Integer>q1;
+    static Queue<Integer>q2;
+    Integer top;
+       MyStack() {
+           q1=new LinkedList<>();
+           q2=new LinkedList<>();
+           
+       }
+       
+       public void push(int x) {
+           q1.add(x);
+           top=x;
+       }
+       
+       public int pop() {
+           int l=q1.size()-1;
+           Integer t=0;
+           while(q2.size()!=l)
+           {
+            //    System.out.println(q1+" "+q2);
+               top=q1.poll();
+               q2.add(top);
+           }
+           t=q1.poll();
+           while(q1.size()!=l)
+           {
+               q1.add(q2.poll());
+            //    top=q2.peek();
+           }
 
 
-
-
-
-
-    public static void main(String[] args)
-    {
-        int[] arr={1,4,2,3,5,6,44,33};
-        Fun s=new Fun(arr);
-         for(int j=0;j<arr.length;j++)
-         {
-            System.out.println(s.fun(j)+"     "+s.list);
-         }
-      
-
+           return t;
+           
+       }
+       
+       public int top() {
+           return top;
+       }
+       
+       public boolean empty() {
+         return q1.isEmpty();  
+       }
+   }
+    public static void main(String[] ags){
+     MyStack s=new MyStack();
+     System.out.println(s.empty());
+     s.push(1);
+     s.push(3);
+     s.push(4);
+     s.push(9);
+     s.pop();
+     s.push(7);
+     s.push(8);
+      System.out.println(s.pop()+" top:"+s.top()+" "+s.empty());
+    // Queue<Integer>p=new LinkedList<>();
+    // p.add(1);
+    // p.add(2);
+    // p.add(3);
+    // Queue<Integer>a=new LinkedList<>();
+    // a.add(p.poll());
+    // a.add(p.poll());
+    // System.out.println(p.size()+" "+a.size());
     }
 }
