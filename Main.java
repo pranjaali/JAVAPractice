e0f57608992dc2a46548ae79cd926b90cf351acd
pranjaali <pranjalisathe390@gmail.com>
2022-07-23 21:41:24 +0530

    bit counting

--- Main.java
+++ Main.java
@@ -1,61 +1,63 @@
-// Java program to merge two sorted arrays
 import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-class Main
-{
-
-	public static void mergeArrays(int[] arr1, int[] arr2, int n1,
-								int n2, int[] arr3)
+public class Main{
+	public static int count(int[] arr)
 	{
-		int i = 0, j = 0, k = 0;
-		while (i<n1 && j <n2)
+		int cnt=0;
+		String[] st=new String[arr.length];
+		
+		for(int i=0;i<st.length;i++)
 		{
-		
-			if (arr1[i] < arr2[j])
-				arr3[k++] = arr1[i++];
-			else
-				arr3[k++] = arr2[j++];
+			st[i]=String.valueOf(decToBinary(arr[i]));
 		}
-		while (i < n1)
-			arr3[k++] = arr1[i++];
-	
-		
-		while (j < n2)
-			arr3[k++] = arr2[j++];
+		int max=0;
+		int[] a=new int[32];
+		for(int i=0;i<st.length;i++)
+		{
+			// System.out.println(st[i]);
+     
+			for(int j=0;j<st[i].length();j++)
+			{
+				Character ch=st[i].charAt(j);
+				if(ch.compareTo('1')==0)
+				{
+					a[j]+=1;
+				}
+
+			}
+		}
+		for(int i=0;i<a.length;i++)
+		{
+			if(max<a[i])
+			{
+				max=a[i];
+			}
+		}
+
+		return max;
 	}
-	
-	public static void main (String[] args)
+	public static String decToBinary(int num)
 	{
-    String[] str={"1,3,4,7,13","1,2,4,13,15"};
-    String[] inputNumber =  str[0].split(",");
-    int arr1 []= new int[inputNumber.length];
-   
-      for(int i=0; i<inputNumber.length;i++){
-            arr1[i]=Integer.parseInt(inputNumber[i]);
-      }
-
-String[] inputNumber1 =  str[0].split(",");
-    int arr2 []= new int[inputNumber.length];
-    
-      for(int i=0; i<inputNumber1.length;i++){
-            arr2[i]=Integer.parseInt(inputNumber1[i]);
-      }
-
-	
-		int n1 = arr1.length;
-	
-		int n2 = arr2.length;
-	
-		int[] arr3 = new int[n1+n2];
+		String s="";
+		while(num!=0)
+		{
+			// System.out.print((num%2));
+			s+=String.valueOf(num%2);
+			num=num/2;
+		}
 		
-		mergeArrays(arr1, arr2, n1, n2, arr3);
-	
-		System.out.println("Array after merging");
-		for (int i=0; i < n1+n2; i++)
-			System.out.print(arr3[i] + " ");
+		return s;
+	}
+	public static void main(String[] args)
+	{
+		Scanner in=new Scanner(System.in);
+		int num=in.nextInt();
+		  int arr[]=new int[num];
+		  for(int i=0;i<num;i++)
+		  {
+			  arr[i]=in.nextInt();
+		  }
+		  int o=count(arr);
+        //  System.out.println(decToBinary(num));
+		System.out.println(o);
 	}
 }
-
-/* This code is contributed by Mr. Somesh Awasthi */
